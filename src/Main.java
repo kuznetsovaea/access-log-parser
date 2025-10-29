79ca6e9b53a04137a74c772b515691dce23ffa7b
Elena Kuznetsova <23-01-1997q@mail.ru>
2025-10-29 18:29:44 +0300

    калькулятор

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
-        }
+        System.out.print("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.print("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        System.out.println("Сумма: " + (firstNumber + secondNumber));
+        System.out.println("Разность: " + (firstNumber - secondNumber));
+        System.out.println("Произведение: " + (firstNumber * secondNumber));
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Частное: " + quotient);
     }
+}
